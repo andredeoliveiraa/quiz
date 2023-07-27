72a423766f9efc5367484875917f8887663ac7b2
André de Oliveira <92805592+andredeoliveiraa@users.noreply.github.com>
2023-07-27 18:56:34 -0300

    Initial commit

---
+++ src/data/questions.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    question: "Qual o menor pais do mundo?",
+    options: [
+      "Brasil",
+      "Váticano",
+      "Republica Tcheca",
+      "Japãp",
+    ],
+    answer: "Váticano",
+  },
+  {
+    question: "Qual a capital da Australia?",
+    options: ["Berlim", "Paris", "Sidney", "Camberra"],
+    answer: "Camberra",
+  },
+  {
+    question: "Quantas faces tem um cubo?",
+    options: ["Uma", "Cinco", "Quatro", "Seis"],
+    answer: "Seis",
+  },
+  {
+    question: "Qual das opções abaixo é um palindromo?",
+    options: ["Casa", "Arroz", "Arara", "Branco"],
+    answer: "Arara",
+  },
+  {
+    question:
+      "Qual o nome da segunda capital do Brasil?",
+    options: ["Manaus", "Rio de Janeiro", "São Paulo", "Salvador"],
+    answer: "Rio de Janeiro",
+  },
+];
+
+export default data;
